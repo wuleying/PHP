bda1302e86f21a34c6db3b7ec40324abfcd44e7f
xuwei <xuwei@dmzj.com>
2017-07-12 16:12:05 +0800

    tianjai l

--- test.php
+++ test.php
@@ -1,12 +1,4 @@
 <?php
 echo "test";
-echo "daadadasdad";
-echo "000";
-echo "888";
-echo "cccccccc";
-echo "999";
-echo "90909090";
-echo "0000000";
-echo "8888dasdasda";
-echo "8888dasdasda";
+echo "lll";
 
